266611956219904d34686bd4e8d2d571173c4bd7
millymike <millymike599@gmail.com>
2023-06-14 00:29:15 +0100

    change service DI

--- PriceIndexing.Api/Program.cs
+++ PriceIndexing.Api/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<BinanceService>();
+builder.Services.AddTransient<BinanceService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
